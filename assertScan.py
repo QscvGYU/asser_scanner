4a827f3746c5018dad89be3b861947561880a86f
baoxx <baoxx@fxiaoke.com>
2020-07-17 17:23:37 +0800

    report

--- assertScan.py
+++ assertScan.py
@@ -1,5 +1,7 @@
 # coding = 'utf-8'
+import json
 
+from ReportTmp.BaseReporterBuilder import BaseReporter
 from core.BaseIPScanner import BaseIPScanner
 from fofa.FofaPortScanner import FofaPortScanner
 from Sublist3r.sublist3r import interactive as sublist_interactive
@@ -10,7 +12,7 @@ def main():
     sub_domain_list = sublist_interactive()
     ip_scanner = BaseIPScanner()
     sub_domain_ip_map = ip_scanner.get_ips(sub_domain_list)
-    scanner_list =  [
+    scanner_list = [
         FofaPortScanner, ShodanPortScanner
     ]
     rst = []
@@ -24,12 +26,12 @@ def main():
                     continue
                 scan_rst_item = scanner.scan(ip_address[4][0])
                 if scan_rst_item:
-                    if ip_scan_rst[sub_domain]:
-                        ip_scan_rst[sub_domain] = ip_scan_rst[sub_domain].extend(scan_rst_item)
+                    if sub_domain in ip_scan_rst:
+                        ip_scan_rst[sub_domain].extend(scan_rst_item)
                     else:
                         ip_scan_rst[sub_domain] = scan_rst_item
             rst.append({scanner.name: ip_scan_rst})
-    print(rst)
+    BaseReporter().build(rst)
 
 
 if __name__ == "__main__":
